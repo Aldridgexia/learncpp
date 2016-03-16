368c02bb36ff937c9df9633b1cf5a3f76e6f565c
alxia <aldridgexia@gmail.com>
2016-03-16 23:40:09 +0800

    practice.

--- learn.cpp
+++ learn.cpp
@@ -194,14 +194,37 @@ using namespace std;
 // 	int age;
 // };
 
-enum class Side{Left,Right};
-enum class Thing{Right,Wrong};
+// enum class Side{Left,Right};
+// enum class Thing{Right,Wrong};
+
+class Point{
+public:
+	Point(int x=0,int y=0):x(x),y(y){count++;}
+	Point(Point &p){x = p.x; y = p.y; count++;}
+	~Point(){count--;}
+	int getX(){return x;}
+	int getY(){return y;}
+	void showCount(){cout<<"Object count = "<<count<<endl;}
+private:
+	int x,y;
+	static int count;
+};
+int Point::count = 0;
 
 int main()
 {
-	Side s = Side::Right;
-	Thing t = Thing::Wrong;
-	cout<<(s == t)<<endl;
+	Point a(4,5);
+	cout<<"Point A:"<<a.getX()<<","<<a.getY()<<endl;
+	a.showCount();
+
+	Point b(a);
+	cout<<"Point B:"<<b.getX()<<","<<b.getY()<<endl;
+	b.showCount();
+	
+
+	// Side s = Side::Right;
+	// Thing t = Thing::Wrong;
+	// cout<<(s == t)<<endl;
 
 	// Student stu = {90071,"linlin",'F',19};
 	// cout << "Num:  " << stu.num << endl;
