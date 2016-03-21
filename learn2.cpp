a213f6e77ccc08ba21f50063194662c117f4005b
alxia <aldridgexia@gmail.com>
2016-03-21 17:34:41 +0800

    new practice.

--- learn2.cpp
+++ learn2.cpp
@@ -6,25 +6,31 @@
 
 using namespace std;
 
-void rowSum(int a[][4],int nRow){
-	for(int i=0;i<nRow;i++){
-		for(int j=0;j<4;j++){
-			a[i][0] = a[i][j];
-		}
-	}
-}
+// void rowSum(int a[][4],int nRow){
+// 	for(int i=0;i<nRow;i++){
+// 		for(int j=0;j<4;j++){
+// 			a[i][0] = a[i][j];
+// 		}
+// 	}
+// }
 
 int main(){
-	int table[3][4] = {{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}};
-	for (int i = 0; i < 3; i++)  {
-         for (int j = 0; j < 4; j++)
-              cout << table[i][j] << "   ";
-         cout << endl;
-     }
-     rowSum(table, 3);  
-     for(int i=0;i<3;i++){
-     	cout << "Sum of row " << i << " is " << table[i][0] << endl;
-     }
+	int array[3] = {1,2,3};
+	for(int &e:array){
+		e += 2;
+		cout<<e<<endl;
+	}
+
+	// int table[3][4] = {{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}};
+	// for (int i = 0; i < 3; i++)  {
+ //         for (int j = 0; j < 4; j++)
+ //              cout << table[i][j] << "   ";
+ //         cout << endl;
+ //     }
+ //     rowSum(table, 3);  
+ //     for(int i=0;i<3;i++){
+ //     	cout << "Sum of row " << i << " is " << table[i][0] << endl;
+ //     }
      
 	// const char key[] = {'a','c','b','a','d'};
 	// const int ques_num = 5;
