eacfedbecab3ca1679ec6a0b8f77529f1aff4019
alxia <aldridgexia@gmail.com>
2016-03-26 13:36:09 +0800

    to be added.

--- learn2.cpp
+++ learn2.cpp
@@ -13,20 +13,21 @@ using namespace std;
 // 		}
 // 	}
 // }
-void splitFloat(float x,int *intPart, float *fracPart){
-	*intPart = static_cast<int>(x);
-	*fracPart = x - *intPart;
+// void splitFloat(float x,int *intPart, float *fracPart){
+// 	*intPart = static_cast<int>(x);
+// 	*fracPart = x - *intPart;
 }
 
 int main(){
-	cout<<"Enter 3 float point numbers: "<<endl;
-	for(int i=0;i<3;i++){
-		float x, f;
-		int n;
-		cin>>x;
-		splitFloat(x, &n, &f);
-		cout<<"Integer part: "<<n<<" Fraction part: "<<f<<endl;
-	}
+	
+	// cout<<"Enter 3 float point numbers: "<<endl;
+	// for(int i=0;i<3;i++){
+	// 	float x, f;
+	// 	int n;
+	// 	cin>>x;
+	// 	splitFloat(x, &n, &f);
+	// 	cout<<"Integer part: "<<n<<" Fraction part: "<<f<<endl;
+	// }
 
 	// int line1[] = {1,2,3};
 	// int line2[] = {4,5,6};
