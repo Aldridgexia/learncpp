97265be8cd5a455dfded5494adca1b741ec74bbb
alxia <aldridgexia@gmail.com>
2016-04-11 15:08:06 +0800

    new practice.

---
+++ chap9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// template <typename T>
+// T abs(T x){
+// 	return x<0?-x:x;
+// }
+
+int main() {
+	// int n = -5;
+	// double d = -6.3;
+	// cout<<abs(n)<<endl;
+	// cout<<abs(d)<<endl;
+
+	return 0;
+}
