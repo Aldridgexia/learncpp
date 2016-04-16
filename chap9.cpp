38881c6f49e08b387533fc224ff6320faae43f54
alxia <aldridgexia@gmail.com>
2016-04-16 16:53:13 +0800

    new practice.

--- chap9.cpp
+++ chap9.cpp
@@ -6,6 +6,21 @@ using namespace std;
 // 	return x<0?-x:x;
 // }
 
+template <class T>
+void insertionSort(T a[], int n){
+	int i,j;
+	T temp;
+	for(int i=1;i<n;i++){
+		int j = i;
+		T temp = a[i];
+		while(j>0 && temp<a[j-1]){
+			a[j] = a[j-1];
+			j--;
+		}
+		a[j] = temp;
+	}
+}
+
 int main() {
 	// int n = -5;
 	// double d = -6.3;
