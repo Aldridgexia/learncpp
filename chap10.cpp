29cf0578326e298ce76bffdac99dfbc536c46159
alxia <aldridgexia@gmail.com>
2016-04-17 19:09:27 +0800

    new practice.

--- chap10.cpp
+++ chap10.cpp
@@ -5,12 +5,39 @@
 #include <functional>
 using namespace std;
 
+// double square(double x){
+// 	return x*x;
+// }
+
+template <class T, class InputIterator, class OutputIterator>
+void mySort(InputIterator first, InputIterator last, OutputIterator result){
+	vector<T> s;
+	for(;first!=last;++first)
+		s.push_back(*first);
+	sort(s.begin(), s.end());
+	copy(s.begin(), s.end(), result);
+}
+
+
 int main(){
-	const int N = 5;
-	vector<int> s(N);
-	for(int i=0;i<N;i++)
-		cin>>s[i];
-	transform(s.begin(), s.end(), ostream_iterator<int>(cout, " "), negate<int>());
+	double a[5] = {1.2, 2.4, 0.8, 3.3, 3.2};
+	mySort<double>(a, a+5, ostream_iterator<double>(cout," "));
 	cout<<endl;
+
+	mySort<int>(istream_iterator<int>(cin), istream_iterator<int>(), ostream_iterator<int>(cout," "));
+	cout<<endl;
+	
+
+
+	// transform(istream_iterator<double>(cin), istream_iterator<double>(), 
+	// 	ostream_iterator<double>(cout,"\t"), square);
+	// cout<<endl;
+
+	// const int N = 5;
+	// vector<int> s(N);
+	// for(int i=0;i<N;i++)
+	// 	cin>>s[i];
+	// transform(s.begin(), s.end(), ostream_iterator<int>(cout, " "), negate<int>());
+	// cout<<endl;
 	return 0;
 }
